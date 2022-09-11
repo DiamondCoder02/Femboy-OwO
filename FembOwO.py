4241edb8644d62d496290b8f4730c11f62c754d0
DiamondPRO02 <kovacsdanielmark@gmail.com>
2022-09-11 21:33:05 +0200

    python homework with discord

--- FembOwO.py
+++ FembOwO.py
@@ -1,24 +1,51 @@
 import discord
 import json
 import os
+import requests
+from discord.ext import commands
+
+os.system('cls')
+
 from dotenv import load_dotenv
-
 load_dotenv()
-
 secret_token = os.getenv("TOKEN")
 
 #json config load
 with open('config.json', 'r') as json_config:
     config = json.load(json_config)
 json_config.close
-# This example requires the 'message_content' intent.
-
-import discord
 
 intents = discord.Intents.default()
 intents.message_content = True
+client = commands.Bot(intents=intents, command_prefix='.')
 
-client = discord.Client(intents=intents)
+@client.command()
+async def facts(ctx, number):
+    print(number)
+    response = requests.get(f'http://numbersapi.com/{number}')
+    await ctx.channel.send(response.text)
+
+# Homework for school:
+@client.command()
+async def calculate_numbers_file(ctx):
+    for attachment in ctx.message.attachments:
+        await attachment.save(attachment.filename)
+    
+    print("\n1.feladat")
+    with open(attachment.filename) as ff:
+        codes=[code.strip() for code in ff]
+        print(codes)
+
+
+
+
+
+
+
+
+
+
+
 
 @client.event
 async def on_ready():
@@ -26,10 +53,12 @@ async def on_ready():
 
 @client.event
 async def on_message(message):
+    await client.process_commands(message)
     if message.author == client.user:
         return
+    if message.content[0] == '.':
+        return
+    await message.channel.send('Hello!')
 
-    if message.content.startswith('$hello'):
-        await message.channel.send('Hello!')
-
-client.run(secret_token)
+if(config["token"] == "token"): client.run(secret_token)
+else: client.run(config["token"])
